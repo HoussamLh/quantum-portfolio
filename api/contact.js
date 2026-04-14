31d93e4953ec46471ffd077936f88f044637d679
HoussamLh <lahlahhoussam80@gmail.com>
2026-04-14 19:01:33 +0100

    docs: finalized vercel routing and configuration

--- api/contact.js
+++ api/contact.js
@@ -8,7 +8,9 @@ app.use(cors());
 app.use(express.json());
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: process.env.EMAIL_HOST,
+  port: Number(process.env.EMAIL_PORT), 
+  secure: true, 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
